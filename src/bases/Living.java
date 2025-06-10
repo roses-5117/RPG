f21d9234f2267e10dc5fcdfcee8eddccd603bc37
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-06-10 23:16:29 +0900

    新規プロジェクト「basses」、新規クラス「Living」を作成

---
+++ src/bases/Living.java
@@ -0,0 +1,11 @@
+package bases;
+
+public class Living {
+	public abstract class Living{
+		
+		protected String name;
+		protected String weapon;
+		protected int hp;
+		protected int offensive;
+	}
+}
