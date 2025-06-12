25727471789cc80c77ad2f0a99d1e663f7064fd6
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-06-12 17:54:47 +0900

    新規クラス作成・コピペ

--- src/bases/Monster.java
+++ src/bases/Monster.java
@@ -1,10 +1,22 @@
 package bases;
 
-public class Monster {
+//モンスターを表現する抽象クラス
+public abstract class Monster extends Living {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	// コンストラクタ
+	public Monster(String name, String weapon) {
+		// Livingクラスで定義したコンストラクタを利用する
 	}
 
+	// attackメソッドのオーバーライド
+	@Override
+	public void attack(Living target) {
+		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
+		
+		// 相手のHPをダメージ値だけ減らす
+		
+		// コンソールにステータスを表示
+		
+	}
 }
+
