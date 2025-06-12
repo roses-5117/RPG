dd2864c2ea4e1492d6b01ca4e46bce5575ff67b4
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-06-12 17:49:25 +0900

    コピペ

--- src/bases/Living.java
+++ src/bases/Living.java
@@ -1,54 +1,63 @@
 package bases;
-//②
-public  abstract class Living {
-		protected String name;
-		protected String weapon;
-		protected int hp;
-		protected int offensive;
-		
-		//③				
-		public String getName() {
-			return name;
-		}
-		
-		public void setName(String name) {
-			this.name=name;
-		}
-		
-		public String getWeapon() {
-			return weapon;
-		}
-		
-		public void setWeapon(String weapon) {
-			this.weapon=weapon;
-		}
-		
-		public int getHp() {
-			return hp;
-		}
-		
-		public void setHp(int hp) {
-			this.hp=hp;
-		}
-	
-		public int getOffesive(int offensive) {
-			return offensive;
-		}
-		
-		public void setOffensive(int offensive) {
-			this.offensive = offensive;
-		}
+//生物を表現する抽象クラス
+public abstract class Living {
 
-	    // ターゲットに攻撃するメソッド
-		public abstract void attack(Living target); 
+	// フィールド
+	protected String name; // キャラクターの名前
+	protected String weapon; // 武器名
+	protected int hp; // ヒットポイント（生命力）
+	protected int offensive; // 攻撃力
 
-		// 自分自身を表現する文字列のオーバーライド
-		@Override
-		public String toString() {
-			String alert = "";
-			if (this.hp <= 50) {
-				alert = "★ HP残りわずか！アブナイ";
-			}
-			return String.format("[名前]: %s, [ヒットポイント]: %d, [攻撃力]: %d %s", this.name, this.hp, this.offensive, alert);
+	// コンストラクタ
+	public Living(String name, String weapon) {
+		this.name = name;
+		this.weapon = weapon;
+	}
+
+	// ゲッター、セッター
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWeapon() {
+		return weapon;
+	}
+
+	public void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getOffensive() {
+		return offensive;
+	}
+
+	public void setOffensive(int offensive) {
+		this.offensive = offensive;
+	}
+
+ // ターゲットに攻撃するメソッド
+	public abstract void attack(Living target); 
+
+	// 自分自身を表現する文字列のオーバーライド
+	@Override
+	public String toString() {
+		String alert = "";
+		if (this.hp <= 50) {
+			alert = "★ HP残りわずか！アブナイ";
 		}
+		return String.format("[名前]: %s, [ヒットポイント]: %d, [攻撃力]: %d %s", this.name, this.hp, this.offensive, alert);
+	}
+
 }
