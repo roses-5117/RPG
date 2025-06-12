6414bd348b0c257e7e6a041670c5a27edfc6a1e4
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-06-12 22:50:26 +0900

    コメントの箇所にコード追加

--- src/bases/Monster.java
+++ src/bases/Monster.java
@@ -6,17 +6,20 @@ public abstract class Monster extends Living {
 	// コンストラクタ
 	public Monster(String name, String weapon) {
 		// Livingクラスで定義したコンストラクタを利用する
+		super(name,weapon);
 	}
 
 	// attackメソッドのオーバーライド
 	@Override
 	public void attack(Living target) {
 		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
-		
+		int dice=(int)(Math.random()*10)+1;
+		int damage=this.offensive*dice;
 		// 相手のHPをダメージ値だけ減らす
-		
+		target.hp-=damage;
 		// コンソールにステータスを表示
-		
+		System.out.println("「" + this.name + "」が「" + this.weapon + "」で攻撃！「" + target.name + "」に" + damage + "のダメージを与えた。");
+
 	}
 }
 
