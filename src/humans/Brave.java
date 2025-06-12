5262cabd7b2bfbfdca228c64cacf43f96d387c88
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-06-12 23:31:21 +0900

    コード追加

--- src/humans/Brave.java
+++ src/humans/Brave.java
@@ -1,10 +1,16 @@
 package humans;
 
-public class Brave {
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import bases.Human;
 
-	}
+public class Brave extends Human {
 
+    public Brave(String name, String weapon) {
+        super(name, weapon);
+
+        Random rand = new Random();
+        this.hp = rand.nextInt(61) + 170;       // 170〜230（=60+1）
+        this.offensive = rand.nextInt(7) + 7;   // 7〜13（=6+1）
+    }
 }
