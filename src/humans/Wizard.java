4126dc2d7d39c474c54733065ffc8e17ce0c3bbe
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-06-13 17:11:41 +0900

    クラス名・数値変更

--- src/humans/Wizard.java
+++ src/humans/Wizard.java
@@ -1,10 +1,16 @@
 package humans;
 
-public class Wizard {
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import bases.Human;
 
-	}
+public class Wizard extends Human {
 
+    public Wizard(String name, String weapon) {
+        super(name, weapon);
+
+        Random rand = new Random();
+        this.hp = rand.nextInt(61) + 120;       // 120〜180（=60+1）
+        this.offensive = rand.nextInt(7) + 12;   // 12〜18（=6+1）
+    }
 }
